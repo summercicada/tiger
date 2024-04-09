688ba48197e71321974bbf5cfce38bf7f5018c3f
HansXiao <hans_l_xiao@manulife.com>
2024-04-09 17:43:33 +0800

    nothing

--- src/main/java/org/summer/tiger/pojo/UserJWT.java
+++ src/main/java/org/summer/tiger/pojo/UserJWT.java
@@ -1,9 +1,11 @@
 package org.summer.tiger.pojo;
 
+import lombok.Builder;
 import lombok.Data;
 import org.summer.tiger.util.JSON;
 
 @Data
+@Builder
 public class UserJWT {
 
     private String username;
--- src/main/java/org/summer/tiger/util/JWTUtil.java
+++ src/main/java/org/summer/tiger/util/JWTUtil.java
@@ -1,23 +1,28 @@
 package org.summer.tiger.util;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.summer.tiger.pojo.UserJWT;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class JWTUtil {
 
     private final static String tokenSecret = "1234123412341234123412341234123412341234123412341234123412341234";
 
     public static String createToken(UserJWT userJwt) {
+        SecretKey secretKey = new SecretKeySpec(tokenSecret.getBytes(), Jwts.SIG.HS256.getId());
         return Jwts.builder()
                 .subject(userJwt.toJson())
-                .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(tokenSecret)))
+                .signWith(secretKey)
                 .compact();
     }
 
 
     public static void main(String[] args) {
-        System.out.println(createToken(null));
+        System.out.println(createToken(UserJWT.builder().username("hello").build()));
     }
 }
